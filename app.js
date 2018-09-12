a742a4c1070e4d61208cd7fb09cfa5ee552d679b
lfoindustries <hello@lfo-industries.com>
2018-09-12 11:36:06 +0200

    Update style efficiency, add brightness slider

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 var filters = {
-    'hue' : 0,
-    'blur': 0,
-    'contrast': 128
+    'hue-rotate' : {
+        'value': 0,
+        'unit': 'deg'
+    },
+    'blur': {
+        'value': 0,
+        'unit': 'px'
+    },
+
+    'contrast': {
+        'value': 128,
+        'unit': '%'
+    },
+    'brightness': {
+        'value': 0,
+        'unit': '%'
+    }
 };
 
+function stampFilterString (filters) {
+
+    return Object.entries(filters).map( ([name, value]) => {
+        return `${name}(${value.value}${value.unit})`;
+    }).join(' ');
+
+}
 
 window.addEventListener("mousemove", function (e) {
     if (e.target.nodeName === 'INPUT') {
 
-        filters[e.target.name] = e.target.value;
+        filters[e.target.name]['value'] = e.target.value;
 
-        var cssFilters = 'blur(' + filters['blur'] + 'px) contrast('+ filters['contrast'] +'%) hue-rotate('+ filters['hue']+'deg)'; 
-        document.querySelector('.shader').setAttribute('style', 'filter: ' + cssFilters);
+        var cssFilters = stampFilterString(filters);
+        //document.querySelector('.shader').setAttribute('style', 'filter: ' + cssFilters);
+        document.querySelector('.shader').style.filter = cssFilters;
     }
 });
